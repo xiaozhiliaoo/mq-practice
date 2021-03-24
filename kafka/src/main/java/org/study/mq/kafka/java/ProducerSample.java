7bf62640833b5183547453e56b759244ecde3bc9
lili <xiaozhiliaoo@gmail.com>
2021-03-24 19:54:09 +0800

    up

--- kafka/src/main/java/org/study/mq/kafka/java/ProducerSample.java
+++ kafka/src/main/java/org/study/mq/kafka/java/ProducerSample.java
@@ -9,7 +9,7 @@ public class ProducerSample {
 
     public static void main(String[] args) {
         Map<String, Object> props = new HashMap<String, Object>();
-        props.put("bootstrap.servers", "localhost:9092");
+        props.put("bootstrap.servers", "192.168.56.101:9092");
         props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 

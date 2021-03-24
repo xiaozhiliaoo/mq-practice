6c21010e596d490df011c8bff22d4fe5c35a55d7
lili <xiaozhiliaoo@gmail.com>
2021-03-24 20:16:14 +0800

    up

--- kafka/src/main/java/org/study/mq/kafka/java/ConsumerSample.java
+++ kafka/src/main/java/org/study/mq/kafka/java/ConsumerSample.java
@@ -13,7 +13,7 @@ public class ConsumerSample {
         String topic = "test-topic";
 
         Properties props = new Properties();
-        props.put("bootstrap.servers", "localhost:9092");
+        props.put("bootstrap.servers", "192.168.56.101:9092");
         props.put("group.id", "testGroup1");
         props.put("enable.auto.commit", "true");
         props.put("auto.commit.interval.ms", "1000");
